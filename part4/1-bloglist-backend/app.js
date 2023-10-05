5c7ee344845e2bf37e426be2b20b67af85c07aa8
gary leong <gary_leong@tech.gov.sg>
2023-10-05 23:29:03 +0800

    4.2 complete

---
+++ part4/1-bloglist-backend/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controller/blog')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+logger.info('connecting to MongoDB', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+// app.use(middleware.requestLogger)
+app.use(middleware.morganLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
